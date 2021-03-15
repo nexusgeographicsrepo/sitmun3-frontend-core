c1f03d5cb6b94dc6fcdfd235bf2dd540f5867251
Jonatan Lucena <jlucena@NG.LOCAL>
2021-03-15 13:46:44 +0100

    dashboard service modified

--- projects/sitmun-frontend-core/src/lib/dashboard/dashboard.model.ts
+++
@@ -1,9 +0,0 @@
-import {Resource} from '../angular-hal/src/lib/resource';
-
-/**
- * Territory type model
- */
-export class Dashboard extends Resource {
-  /** id */
-  public id: number;  
-}
--- projects/sitmun-frontend-core/src/lib/dashboard/dashboard.service.ts
+++ projects/sitmun-frontend-core/src/lib/dashboard/dashboard.service.ts
@@ -1,21 +1,24 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { RestService } from '../angular-hal/src/lib/rest.service';
-import { Dashboard } from './dashboard.model';
-
+import {ResourceService} from '../angular-hal/src/lib/resource.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DashboardService extends RestService<Dashboard> {
+export class DashboardService{
 
     /** API resource path */
     public DASHBOARD_API = 'dashboard/info';
-
+    public DASHBOARD_EMBEDDED= 'dashboard';
     /** constructor */
-    constructor(injector: Injector,private http: HttpClient) {
-      super(Dashboard, "dashboard/info", injector);
+    constructor(       
+      private http: HttpClient,
+      private resourceService: ResourceService) {
     }
-    
+  
+    /** get all kpi */
+    getAll(): Observable<any> {
+      return this.http.get(this.resourceService.getResourceUrl(this.DASHBOARD_API),{observe : 'response'}).map(response => response[this.DASHBOARD_EMBEDDED]);
+    }
 }
